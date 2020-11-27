c0afc358dddcca891e2bbcb5ebe3cfffd54c2c3c
kuudr <alxkdr@icloud.com>
2020-11-27 15:54:15 +0300

    DB create

---
+++ data/dbConn.php
@@ -0,0 +1,12 @@
+<?php
+
+
+class dbConn
+{
+    const HOST = 'localhost';
+    const DB_NAME = 'imagespark';
+    const DB_USER = 'homestead';
+    const DB_PASSWORD = 'secret';
+    const SHOW_ERRORS = 'true';
+
+}
